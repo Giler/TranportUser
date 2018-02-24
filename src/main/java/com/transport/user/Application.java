6a3920df0c35847734a02bdae2b3bd549a1e95d5
Rain <276161983@qq.com>
2018-02-24 22:20:24 +0800

    test

--- src/main/java/com/transport/user/Application.java
+++ src/main/java/com/transport/user/Application.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // 程序启动入口
+        // 程序启动入口 ttes
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class,args);
     }
